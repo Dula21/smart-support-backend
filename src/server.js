46db513121838af4c70ef44020c31502fcdf3a38
Dula21 <nethmadulasi15@gmail.com>
2026-02-20 15:06:06 +0400

    change the uploads directory issues

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
 require('dotenv').config();
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,15 +11,23 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));  // Serve files at /uploads/filename.jpg
+
+
+// Serve static files from uploads directory
+app.use("/uploads", express.static(path.resolve("uploads")));
+
+
 
 // Connect to DB first, then load routes
 connectDB().then(() => {
   const authRoutes = require('./routes/auth-routes');
   const ticketRoutes = require('./routes/ticket-routes');
+  const userRoutes = require('./routes/user-routes');
+
 
   app.use('/api/auth', authRoutes);
   app.use('/api/tickets', ticketRoutes);
+  app.use('/api/users', userRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
