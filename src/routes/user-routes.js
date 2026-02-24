22f0d69995ed1fb0e6667f347b795429438c61d9
Dula21 <nethmadulasi15@gmail.com>
2026-02-24 12:58:56 +0400

    Modified the code

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -3,6 +3,8 @@ const { getAdmins } = require('../controllers/user-controller');
 const { authenticate, authorize } = require('../middleware/auth-middleware');
 const router = express.Router();
 
-router.get('/admins', authenticate, authorize(['admin']), getAdmins);  // Only admins can fetch admins
+
+router.get('/admins', authenticate, getAdmins); 
+  // Only admins can fetch admins
 
 module.exports = router;
