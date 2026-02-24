3ddfc13012741d434d932fffb07f78ee41706df7
Dula21 <nethmadulasi15@gmail.com>
2026-02-24 12:47:21 +0400

    Import dns

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forces Node to use Google's DNS
+
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
