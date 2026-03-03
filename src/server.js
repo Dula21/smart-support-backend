9d5bf5a4ffd058a970d54ac8e76a993060c5cfe4
Dula21 <nethmadulasi15@gmail.com>
2026-03-03 19:10:50 +0400

    Update the port variables

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // --- UPDATED CORS SECTION ---
 const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:3000';
@@ -36,7 +36,7 @@ connectDB().then(() => {
   app.use('/api/tickets', ticketRoutes);
   app.use('/api/users', userRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 }).catch((error) => {
